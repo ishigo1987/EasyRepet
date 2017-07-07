cd0d2952729607c58d6757b6ccda484c9d2b21ec
ishigo1987 <stephnguets@yahoo.fr>
2017-07-07 11:43:27 +0100

    Changement mineur

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <title>HomeFood</title>
+  <title>EasyRepet</title>
   <meta http-equiv="Content-type" content="text/html; charset=utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="robots" content="index, follow">
   <meta name="viewport" content="width=device-width, height=device-height, minimum-scale=1.0, initial-scale=1, maximum-scale=3 user-scalable=no">
   <meta name="description" content="Plateforme de presentation et recherche de répétiteurs en ligne a Yaoundé">
   <meta name="author" content="Nguetseng Stephane">
-  <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="icons/favicon.ico" type="image/x-icon">
  <!--  <link rel="stylesheet" href="https://cdn.rawgit.com/ishigo1987/assetsHomeFood/f9905365/css/index.css" media="all">
   <link rel="stylesheet" href="https://cdn.rawgit.com/ishigo1987/assetsHomeFood/f9905365/css/mobile.css" media="all"> -->
   <link rel="stylesheet" href="css/index.css" media="all">
